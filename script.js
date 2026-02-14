df75a9466c8978746264314094ff03b138285f61
Markandey Shukla <117027551+markandeyshukla@users.noreply.github.com>
2026-02-15 05:17:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let btnClick=document.getElementById("enterBtn");
+let textElement=document.getElementById("status");
+
+
+btnClick.addEventListener("click",()=>{
+	textElement.innerHTML = "<h1>Entered Metaversed</h1>";
+})
